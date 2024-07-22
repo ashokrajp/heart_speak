44f616650d6dd004ec8a7d8cbde339b3f97a62d0
ashokr <ashokr@hyperlinkinfosystem.net.in>
2024-07-22 14:41:47 +0530

    first commit

---
+++ modules/Cron/route.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const cron = require('node-cron');
+const cron_model = require('./cron_model');
+
+
+/*------------- CHECK FOR ADMIN TOKEN EXPIRATION -------------*/
+cron.schedule('5 0 * * *', () => {
+    console.log('CHECKING FOR ADMIN TOKEN EXPIRATION...');
+    cron_model.checkDeleteChatHistory();
+});
+
+
+
+module.exports = router;
