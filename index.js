432149a793f8cac330e93f3711e190e34cbd168c
ashokr <ashokr@hyperlinkinfosystem.net.in>
2024-12-13 18:16:57 +0530

    again commit

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 require('./config/database');
 let express = require('express')
-const cors = require('cors')
 const bodyParser = require('body-parser')
     
 let app = express();
@@ -10,16 +9,24 @@ let PORT = process.env.PORT || 9000;
 let authentication = require('./modules/v1/Authentications/route_manager');
 let services = require('./modules/v1/Authentications/route_manager');
 
+
+const httpServer = require('http').createServer(app);
+const io = require('socket.io')(httpServer, {
+    cors: { origin: '*' }
+});
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 // app.use(bodyParser.json({ limit: '10mb' }));
 // app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: false }));
+// app.use(express.json({ limit: '10mb' }));
+// app.use(express.urlencoded({ limit: '10mb', extended: false }));
 
 var cors = require('cors')
 app.use(cors())
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use("/api/services/", authentication);
 app.use("/v1/service", services);
 
